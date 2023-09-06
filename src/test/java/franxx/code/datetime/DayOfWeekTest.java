8aa75a74416f021a8bed8ccdf30c2045b8b60def
HILMI AM <imiia75775@gmail.com>
2023-09-06 23:14:54 +0700

    DayOfWeekTest

---
+++ src/test/java/franxx/code/datetime/DayOfWeekTest.java
@@ -0,0 +1,19 @@
+package franxx.code.datetime;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.DayOfWeek;
+
+public class DayOfWeekTest {
+    @Test
+    void create() {
+        DayOfWeek dayOfWeek1 = DayOfWeek.SUNDAY;
+        DayOfWeek dayOfWeek2 = dayOfWeek1.plus(1);
+        DayOfWeek dayOfWeek3 = dayOfWeek1.plus(5);
+
+        System.out.println(dayOfWeek1);
+        System.out.println(dayOfWeek2);
+        System.out.println(dayOfWeek3);
+
+    }
+}
